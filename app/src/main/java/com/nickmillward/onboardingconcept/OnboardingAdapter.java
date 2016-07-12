8d60315693f97679dc40a3de2614fb3d16fed77a
Nick Millward <nicholasmillward@gmail.com>
2016-07-12 17:51:43 -0400

    set adapter to create new fragment instances

--- app/src/main/java/com/nickmillward/onboardingconcept/OnboardingAdapter.java
+++ app/src/main/java/com/nickmillward/onboardingconcept/OnboardingAdapter.java
@@ -1,5 +1,6 @@
 package com.nickmillward.onboardingconcept;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -15,11 +16,20 @@ public class OnboardingAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        switch (position) {
+            case 0:
+                return OnboardingFragment.newInstance(Color.parseColor("#03A9F4"), position);
+            case 1:
+                return OnboardingFragment.newInstance(Color.parseColor("#00BCD4"), position);
+            case 2:
+                return OnboardingFragment.newInstance(Color.parseColor("#009688"), position);
+            default:
+                return OnboardingFragment.newInstance(Color.parseColor("#03A9F4"), position);
+        }
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return 3;
     }
 }
