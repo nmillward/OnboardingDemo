49a7b34eaff6bd3f719583e07e25296d2800c5db
Nick Millward <nicholasmillward@gmail.com>
2016-07-12 17:37:57 -0400

    add onboarding page transformer

---
+++ app/src/main/java/com/nickmillward/onboardingconcept/OnboardingPageTransformer.java
@@ -0,0 +1,14 @@
+package com.nickmillward.onboardingconcept;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+/**
+ * Created by nmillward on 7/12/16.
+ */
+public class OnboardingPageTransformer implements ViewPager.PageTransformer {
+    @Override
+    public void transformPage(View page, float position) {
+        
+    }
+}
