b35560db7ecf55b8acd86fadbdc1e2605a378736
Nick Millward <nicholasmillward@gmail.com>
2016-07-12 16:59:18 -0400

    change MainActivity to OnboardingActivity

--- app/src/main/java/com/nickmillward/onboardingconcept/OnboardingActivity.java
+++ app/src/main/java/com/nickmillward/onboardingconcept/OnboardingActivity.java
@@ -3,7 +3,7 @@ package com.nickmillward.onboardingconcept;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class OnboardingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
