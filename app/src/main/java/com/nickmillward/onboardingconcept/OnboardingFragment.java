9e1b48c6775f0885d7d31d13e5486343dfbaf9a4
Nick Millward <nicholasmillward@gmail.com>
2016-07-13 17:43:51 -0400

    route fragment layout switch statement. add ids for fragment bg

--- app/src/main/java/com/nickmillward/onboardingconcept/OnboardingFragment.java
+++ app/src/main/java/com/nickmillward/onboardingconcept/OnboardingFragment.java
@@ -15,6 +15,9 @@ public class OnboardingFragment extends Fragment {
     private static final String BACKGROUND_COLOR = "background_color";
     private static final String PAGE = "page";
 
+    private int backgroundColor;
+    private int page;
+
     public static OnboardingFragment newInstance(int backgroundColor, int page) {
         OnboardingFragment fragment = new OnboardingFragment();
 
@@ -30,16 +33,47 @@ public class OnboardingFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        backgroundColor = getArguments().getInt(BACKGROUND_COLOR);
+        page = getArguments().getInt(PAGE);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        // Select a layout according to the current page
+        int layoutResId;
+        switch (page) {
+            case 0:
+                layoutResId = R.layout.fragment_layout_01;
+                break;
+            case 1:
+                layoutResId = R.layout.fragment_layout_02;
+                break;
+            case 2:
+                layoutResId = R.layout.fragment_layout_03;
+                break;
+            default:
+                layoutResId = R.layout.fragment_layout_01;
+                break;
+        }
+
+        // Inflate layout resource
+        View view = getActivity().getLayoutInflater().inflate(layoutResId, container, false);
+
+        // Set the current page index as the View's tag (used for PageTransformer)
+        view.setTag(page);
+
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Set the background color of the root view to the color specified in newInstance()
+        View background = view.findViewById(R.id.onboarding_fragment_bg);
+        background.setBackgroundColor(backgroundColor);
     }
 }
