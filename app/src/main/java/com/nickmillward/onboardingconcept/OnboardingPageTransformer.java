3c7d487841c5adeea1fafaa3c335fdc113a96dae
Nick Millward <nicholasmillward@gmail.com>
2016-07-31 13:15:49 -0400

    set animation for feature elements

--- app/src/main/java/com/nickmillward/onboardingconcept/OnboardingPageTransformer.java
+++ app/src/main/java/com/nickmillward/onboardingconcept/OnboardingPageTransformer.java
@@ -78,9 +78,22 @@ public class OnboardingPageTransformer implements ViewPager.PageTransformer {
 
 
             // Fragment 3 -- Feature
-//            if (profile != null) profile.setScaleX(1.0f - absPosition * 2);
-//            if (profile != null) profile.setScaleY(1.0f - absPosition * 2);
-//            if (profile != null) profile.setAlpha(1.0f - absPosition * 2);
+            final View tv_feature_title = page.findViewById(R.id.tv_feature_title);
+            if (tv_feature_title != null) tv_feature_title.setAlpha(1.0f - absPosition * 2);
+
+            final View bike = page.findViewById(R.id.iv_bike);
+            if (bike != null) {
+                bike.setScaleX(1.0f - absPosition * 2);
+                bike.setScaleY(1.0f - absPosition * 2);
+                bike.setAlpha(1.0f - absPosition * 2);
+            }
+
+            final View bike_shadow = page.findViewById(R.id.iv_bike_shadow);
+            if (bike_shadow != null) {
+                bike_shadow.setScaleX(1.0f - absPosition * 2);
+                bike_shadow.setScaleY(1.0f - absPosition * 2);
+                bike_shadow.setAlpha(1.0f - absPosition * 2);
+            }
 
         }
 
